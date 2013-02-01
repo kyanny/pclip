77a68155ef587baa8a9932c21d7988e3127e577e
Kensuke Nagae <kyanny@gmail.com>
2013-02-01 10:39:52 +0900

    Paperclip を使ったサンプルアプリケーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :path => "#{Rails.root}/../images/:class/:attachment/:id_partition/:style/:filename"
 end
